43c0cc98194e91c92f8036e6fbf0133b464360ca
blerimcazimi <lenovo12>
2016-11-24 10:28:01 +0100

    Lidt gode database

--- src/com/company/MembersInformation.java
+++ src/com/company/MembersInformation.java
@@ -1,5 +1,10 @@
 package com.company;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  * Created by Thomas on 21-11-2016.
  */
@@ -8,9 +13,25 @@ public class MembersInformation {
     private String memberName;
     private int memeberAge;
 
-    public MembersInformation(String name, int age) {
+    public MembersInformation(String name, int age)
+    {
         this.memberName = name;
         this.memeberAge = age;
+
+        try
+        {
+            Database database = new Database();
+            Connection conn = database.getConnection();
+
+            Statement stmt = conn.createStatement() ;
+            String query = "insert into member SET navn = '" + name + "', alder = '" + age + "'";
+            stmt.executeUpdate(query);
+
+        } catch(SQLException a)
+        {
+            System.out.println("lol");
+        }
+
     }
 
     public int getMemeberAge() {
@@ -20,4 +41,5 @@ public class MembersInformation {
     public String getMemberName() {
         return memberName;
     }
+
 }
